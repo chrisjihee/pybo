720b991f6bafbe77d91c40d09a4a7be836b5dcd7
Jihee Ryu <chrisjihee@etri.re.kr>
2024-10-07 20:39:51 +0900

    Use application factory

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,11 @@
 from flask import Flask
 
-app = Flask(__name__)
 
+def create_app():
+    app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+    @app.route('/')
+    def hello_world():
+        return 'Hello World!'
 
-
-if __name__ == '__main__':
-    app.run()
+    return app
