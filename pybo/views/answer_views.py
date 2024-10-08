29579def1a9adc290fb41156dd8723d7e2c56f86
Jihee Ryu <chrisjihee@etri.re.kr>
2024-10-08 14:40:31 +0900

    Update minor

--- pybo/views/answer_views.py
+++ pybo/views/answer_views.py
@@ -10,11 +10,11 @@ from ..models import Question, Answer
 bp = Blueprint('answer', __name__, url_prefix='/answer')
 
 
-@bp.route('/create/<int:question_id>', methods=('POST',))
+@bp.route('/create/<int:question_id>', methods=('GET', 'POST'))
 def create(question_id):
     form = AnswerForm()
     question = Question.query.get_or_404(question_id)
-    if form.validate_on_submit():
+    if request.method == 'POST' and form.validate_on_submit():
         content = request.form['content']
         answer = Answer(content=content, create_date=datetime.now())
         question.answer_set.append(answer)
